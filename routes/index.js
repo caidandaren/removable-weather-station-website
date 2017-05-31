1597d2ea9a0af2d2cc03e0193299ebc9a046aedb
caidandaren <caidan_pe@outlook.com>
2017-06-01 07:02:44 +0800

    v0.1

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var cassandra = require('cassandra-driver');
+var assert = require('assert');
+var client = new cassandra.Client({ contactPoints: ['localhost'],keyspace: 'zapdos'});
+/* GET home page. */
+
+router.get('/realTime', function(req, res, next) {
+    client.execute('select value,ts from data_month where key = 3 and month = 569 ORDER BY ts DESC limit 1 ;', function (err, result) {
+        if (err) {
+            return console.error('There was while trying to retrieve data from system.local', err);
+        }else {
+            Hum=result.toString();
+        }
+    });
+    client.execute('select value,ts from data_month where key = 4 and month = 569 ORDER BY ts DESC limit 1 ;', function (err, result) {
+        if (err) {
+            return console.error('There was while trying to retrieve data from system.local', err);
+        }else {
+            Tem=result.toString();
+        }
+    });
+    client.execute('select value,ts from data_month where key = 5 and month = 569 ORDER BY ts DESC limit 1 ;', function (err, result) {
+        if (err) {
+            return console.error('There was while trying to retrieve data from system.local', err);
+        }else {
+            Lux=result.toString();
+        }
+    });
+    res.render('index',{Hum:Hum , Tem: Tem, Lux: Lux, title: 'hhss'});
+});
+
+router.get('/past', function(req, res, next) {
+    client.execute('select * from user', function (err, result) {
+        if (err) {
+            return console.error('There was while trying to retrieve data from system.local', err);
+        }else {
+            res.render('past',{result:result.toString(), title: 'hhss'});
+        }
+    });
+});
+
+module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,20 @@
-/**
- * Created by caidan on 5/30/17.
- */
+var express = require('express');
+var router = express.Router();
+var assert = require('assert');
+
+router.route("/test").get(function(req,res){    // 到达此路径则渲染login文件，并传出title值供 login.html使用
+    res.render("login",{title:'User Login'});
+}).post(function(req,res){
+    var uname = req.body.uname;                //获取post上来的 data数据中 uname的值
+    var upwd = req.body.upwd;
+    if(uname == 'sjtu' && upwd == '4a') {
+        console.log(upwd);
+        res.send(200);
+        res.session.user = 'sjtu';
+    }
+    else{
+        res.send(404);
+    }
+});
+
+module.exports = router;
