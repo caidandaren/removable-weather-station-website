add83d9fd3a1e8a859d0741167809fede3c885ec
caidandaren <caidan_pe@outlook.com>
2017-06-01 20:26:11 +0800

    v2.0

--- routes/index.js
+++ routes/index.js
@@ -42,11 +42,46 @@ router.get('/realTime', function(req, res, next) {
 });
 
 router.get('/past', function(req, res, next) {
-    client.execute('select * from user', function (err, result) {
+    var Hum,Tem,Lum;
+    client.execute('select value,ts from data_month where key = 2 and month = 570 ORDER BY ts DESC limit 60 ;', function (err, result) {
         if (err) {
             return console.error('There was while trying to retrieve data from system.local', err);
         }else {
-            res.render('past',{result:result.toString(), title: 'hhss'});
+            for (var i = 0 ; i <10;i++)
+            {
+                Hum[i]=result.rows[6*i].value;
+            }
+        }
+    });
+    client.execute('select value,ts from data_month where key = 3 and month = 570 ORDER BY ts DESC limit 1 ;', function (err, result) {
+        if (err){
+            return console.error('There was while trying to retrieve data from system.local', err);
+        }else {
+            for (var i = 0 ; i <10;i++)
+            {
+                Tem[i]=result.rows[6*i].value;
+            }
+        }
+    });
+    client.execute('select value,ts from data_month where key = 4 and month = 570 ORDER BY ts DESC limit 1 ;', function (err, result) {
+        if (err) {
+            return console.error('There was while trying to retrieve data from system.local', err);
+        }else {
+            for (var i = 0 ; i <10;i++)
+            {
+                Lum[i]=result.rows[6*i].value;
+                if(Lum[i]>50 && Hum[i]>70){
+                    Wea[i]='雨';}
+                else if(Lum[i]>50){
+                    Wea[i]='阴';}
+                else{
+                    Wea[i]='晴';}
+                Time[i]=result.rows[6*i].ts;
+            }
+            Car=95;
+            if(Hum !=undefined&& Tem != undefined){
+                res.render('index',{Hum:Hum, Tem:Tem, Lum:Lum, Car:Car, Wea:Wea,Time:Time, title:'realtime'});
+            }
         }
     });
 });
